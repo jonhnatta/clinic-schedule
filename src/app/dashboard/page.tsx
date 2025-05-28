b830409ffe33bd0c5d438165e5be000282a2d4cc
jonhnatta <jonhnatta.santos@gmail.comclear>
2025-05-28 03:25:38 -0300

    feat: add clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,14 +2,27 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import SignOutButton from "./components/sign-out-button";
 import { redirect } from "next/navigation";
+import { db } from "@/db"; // Assuming you have a db instance set up
+import { usersToClinicsTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const DashboardPage = async () => {
     const session = await auth.api.getSession({
         headers: await headers(),
     });
+
     if(!session?.user){
         redirect("/authentication");
     }
+
+    const clinics = await db.query.usersToClinicsTable.findMany({
+        where: eq(usersToClinicsTable.userId, session.user.id),
+    })
+
+    if( clinics.length === 0) {
+        redirect("/clinic-form");
+    }
+
     return (
         <div>
             <h1>Dashboard</h1>
