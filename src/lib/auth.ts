0459560869ecd68d4c7684c9e8ef02d75c8d6104
jonhnatta <jonhnatta.santos@gmail.comclear>
2025-05-28 03:47:46 -0300

    feat: sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,27 +1,34 @@
-import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { db } from "@/db";
 import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg", // or "pg" or "mysql"
-    usePlural: true, // Use plural table names
-    schema: schema, // Import your schema from the database
+    provider: "pg",
+    usePlural: true,
+    schema,
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   user: {
-    modelName: "usersTable", // Name of the user model
+    modelName: "usersTable",
   },
   session: {
-    modelName: "sessionsTable", // Name of the session model
+    modelName: "sessionsTable",
   },
   account: {
-    modelName: "accountsTable", // Name of the account model
+    modelName: "accountsTable",
   },
   verification: {
-    modelName: "verificationsTable", // Name of the verification model
+    modelName: "verificationsTable",
   },
-  emailAndPassword: { 
-    enabled: true, 
-  }, 
+  emailAndPassword: {
+    enabled: true,
+  },
 });
