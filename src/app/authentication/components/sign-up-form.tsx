63501e89ff0c0c52784023e9a0d2248ec48eb5ab
jonhnatta <jonhnatta.santos@gmail.comclear>
2025-05-27 23:27:52 -0300

    feat: add sign-up

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -16,9 +16,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import z from "zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const registerSchema = z.object({
   name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
@@ -34,6 +37,7 @@ const registerSchema = z.object({
 });
 
 const SignUpForm = () => {
+  const router = useRouter();
   const form = useForm<z.infer<typeof registerSchema>>({
     resolver: zodResolver(registerSchema),
     defaultValues: {
@@ -43,8 +47,16 @@ const SignUpForm = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof registerSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof registerSchema>) {
+    await authClient.signUp.email({
+      email: values.email,
+      password: values.password,
+      name: values.name
+    }, {
+        onSuccess: () => {
+          router.push("/dashboard");
+        }
+    });
   }
 
   return (
@@ -103,8 +115,16 @@ const SignUpForm = () => {
             />
           </CardContent>
           <CardFooter>
-            <Button className="w-full" type="submit">
-              Criar conta
+            <Button
+              className="w-full"
+              type="submit"
+              disabled={form.formState.isSubmitting}
+            >
+              {form.formState.isSubmitting ? (
+                <Loader2 className="w4 h4 mr-2 animate-spin" />
+              ) : (
+                "Criar Conta"
+              )}
             </Button>
           </CardFooter>
         </form>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,13 @@
 import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "pg" or "mysql"
     usePlural: true, // Use plural table names
+    schema: schema, // Import your schema from the database
   }),
   user: {
     modelName: "usersTable", // Name of the user model
@@ -19,4 +21,7 @@ export const auth = betterAuth({
   verification: {
     modelName: "verificationsTable", // Name of the verification model
   },
+  emailAndPassword: { 
+    enabled: true, 
+  }, 
 });
